356b8e5496d78630710c2906c95092844ac3f029
ychernookiy <30776761+ychernookiy@users.noreply.github.com>
2023-03-29 18:03:59 +0300

    update js

--- prime_invest/testportfolio/filters.js
+++ prime_invest/testportfolio/filters.js
@@ -2,6 +2,8 @@ const filterContainer = document.querySelector(".gallery-filter"),
   galleryItems = document.querySelectorAll(".gallery-item");
 
 const filterContainer2 = document.querySelector(".gallery-filter2");
+let currentFilter1 = "all";
+let currentFilter2 = "all";
 
  filterContainer.addEventListener("click", (event) =>{
    if(event.target.classList.contains("filter-item")){
@@ -10,15 +12,44 @@ const filterContainer2 = document.querySelector(".gallery-filter2");
      // activate new 'filter-item'
      event.target.classList.add("active");
      const filterValue = event.target.getAttribute("data-filter");
+     currentFilter1 = filterValue;
      galleryItems.forEach((item) => {
-       if(item.classList.contains(filterValue) || filterValue === 'all'){
-        item.classList.remove("hide");
-        item.classList.add("show");
-       }
-       else{
-        item.classList.remove("show");
-        item.classList.add("hide");
-       }
+      if(filterValue !== 'all') {
+        if (currentFilter2 !== "all") {
+          if((item.classList.contains(filterValue) && item.classList.contains(currentFilter2))){
+            item.classList.remove("hide");
+            item.classList.add("show");
+          } else{
+            item.classList.remove("show");
+            item.classList.add("hide");
+          }
+        } else {
+          if(item.classList.contains(filterValue)){
+            item.classList.remove("hide");
+            item.classList.add("show");
+           }
+           else{
+            item.classList.remove("show");
+            item.classList.add("hide");
+           }
+        }
+      } else {
+        if(filterValue === 'all' && item.classList.contains(currentFilter2) && currentFilter2 !== "all"){
+          item.classList.remove("hide");
+          item.classList.add("show");
+         }
+         else{
+          if(filterValue === 'all' && currentFilter2 === "all"){
+            item.classList.remove("hide");
+            item.classList.add("show");
+          }
+          else {
+            item.classList.remove("show");
+            item.classList.add("hide");
+          }
+         }
+      }
+       
      });
    }
 
@@ -32,14 +63,42 @@ const filterContainer2 = document.querySelector(".gallery-filter2");
     // activate new 'filter-item'
     event.target.classList.add("active");
     const filterValue = event.target.getAttribute("data-filter");
+    currentFilter2 = filterValue;
     galleryItems.forEach((item) => {
-      if(item.classList.contains(filterValue) || filterValue === 'all'){
-       item.classList.remove("hide");
-       item.classList.add("show");
-      }
-      else{
-       item.classList.remove("show");
-       item.classList.add("hide");
+      if(filterValue !== 'all') {
+        if (currentFilter1 !== "all") {
+          if((item.classList.contains(filterValue) && item.classList.contains(currentFilter1))){
+            item.classList.remove("hide");
+            item.classList.add("show");
+          } else{
+            item.classList.remove("show");
+            item.classList.add("hide");
+          }
+        } else {
+          if(item.classList.contains(filterValue)){
+            item.classList.remove("hide");
+            item.classList.add("show");
+           }
+           else{
+            item.classList.remove("show");
+            item.classList.add("hide");
+           }
+        }
+      } else {
+        if(filterValue === 'all' && item.classList.contains(currentFilter1) && currentFilter1 !== "all"){
+          item.classList.remove("hide");
+          item.classList.add("show");
+         }
+         else{
+            if(filterValue === 'all' && currentFilter1 === "all"){
+              item.classList.remove("hide");
+              item.classList.add("show");
+            }
+            else {
+              item.classList.remove("show");
+              item.classList.add("hide");
+            }
+         }
       }
     });
   }
